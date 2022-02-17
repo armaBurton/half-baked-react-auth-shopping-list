78c1ed0a0c7942cc996d135bb66314dd8045ff6b
armaBurton <armanarma@armaburton.com>
2022-02-16 19:23:07 -0800

    testing passed

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import AuthPage from './AuthPage';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('The shopping list app For a busy world', () => {
+  render(<AuthPage />);
+  const linkElement = screen.getByText(/list app For/i);
   expect(linkElement).toBeInTheDocument();
 });
